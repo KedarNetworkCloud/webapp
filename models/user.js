91e2c3462437327bb47107d935aa93e740e5b847
aptekedar <apte.k@northeastern.edu>
2024-10-01 10:43:22 -0400

    Assignment 2 code changes

--- models/user.js
+++ models/user.js
@@ -1,56 +1,49 @@
 const { DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 const sequelize = require('../config/config.js');
 
 const User = sequelize.define('User', {
     id: {
-        type: DataTypes.UUID, // Use UUID type
-        defaultValue: DataTypes.UUIDV4, // Automatically generates UUIDv4
-        primaryKey: true, // Set as the primary key
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
     },
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-    //Here we are using isEmail validator provided by sequelize by default along with some others such
-    //as isAlphanumeric, isNumeric, isUrl
-    validate: {
-      isEmail: true,
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            isEmail: true,
+        },
     },
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  account_created: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-  account_updated: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    account_created: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+    account_updated: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
 }, {
-  timestamps: false,
-  tableName:'User',
-  hooks: {
-    beforeCreate: async (user) => {
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(user.password, salt);
+    timestamps: false,
+    tableName: 'User',
+    hooks: {
+        beforeUpdate: (user) => {
+            user.account_updated = new Date(); // Update timestamp on user update
+        }
     },
-    beforeUpdate: (user) => {
-      user.account_updated = new Date();
-    }
-  },
-  schema: 'public', 
+    schema: 'public', 
 });
 
 module.exports = User;
